76ddd251bef709302480de7206dc124171a18f28
volkov.al <volkov.al@tecomgroup.ru>
2024-09-29 10:51:14 +0300

    init app blender save path

---
+++ blender_save_path_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlenderSavePathProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blender_save_path_project'
