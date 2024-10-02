e62515f68db669d41d87b5d4c22208db1331ab30
volkov.al <volkov.al@tecomgroup.ru>
2024-10-02 15:05:47 +0300

    update settings

--- astudio_site/astudio_site/settings.py
+++ astudio_site/astudio_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-obf8u3l6&smbct%%vhxa9k(p1xr9&3hr2ufi*3vd=w&9e%o*a='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
